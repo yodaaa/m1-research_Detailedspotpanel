1b15f6310c63c5847362d59b3d87140be877e065
yodaaa <yodaaa@macbookproyodaaa.local>
2018-06-05 13:42:36 +0900

    Initial Commit

---
+++ paneltest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paneltest
+//
+//  Created by yodaaa on 2018/06/05.
+//  Copyright © 2018年 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
